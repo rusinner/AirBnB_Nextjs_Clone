e376a6b42f7c98fea90a25b01b3b0b7f461c1ba1
Theologos Mavropoulos <rusinner88@gmail.com>
2023-05-10 01:33:43 +0300

    folder structure

---
+++ airbnb-clone/app/layout.tsx
@@ -0,0 +1,25 @@
+import Navbar from "./components/navbar/Navbar";
+import "./globals.css";
+import { Nunito } from "next/font/google";
+
+const font = Nunito({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "AirBnB",
+  description: "AirBnB Clone",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
