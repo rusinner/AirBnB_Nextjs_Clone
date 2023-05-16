bf5999e5effb1778d5b374860ea5e33d8b76bbbc
Theologos Mavropoulos <rusinner88@gmail.com>
2023-05-16 10:14:16 +0300

    categories

--- airbnb-clone/app/components/modals/RegisterModal.tsx
+++ airbnb-clone/app/components/modals/RegisterModal.tsx
@@ -13,6 +13,7 @@ import Modal from "./Modal";
 import Heading from "../Heading";
 import Input from "../inputs/Input";
 import Button from "../Button";
+import { signIn } from "next-auth/react";
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal();
@@ -83,13 +84,13 @@ const RegisterModal = () => {
         outline
         label="Continue with Google"
         icon={FcGoogle}
-        onClick={() => {}}
+        onClick={() => signIn("google")}
       />
       <Button
         outline
         label="Continue with GitHub"
         icon={AiFillGithub}
-        onClick={() => {}}
+        onClick={() => signIn("github")}
       />
       <div className="text-neutral-500 text-500 mt-4 font-light">
         <div className="justify-center flex flex-row items-center gap-2">
