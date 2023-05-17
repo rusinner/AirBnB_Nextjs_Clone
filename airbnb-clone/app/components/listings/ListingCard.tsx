ea8a8788c492297b4bce8df6f298d4c0f9de7e7e
Theologos Mavropoulos <rusinner88@gmail.com>
2023-05-17 19:59:57 +0300

    ListingCard

---
+++ airbnb-clone/app/components/listings/ListingCard.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+interface ListingcardProps {
+  data: any;
+}
+
+const ListingCard: React.FC<ListingcardProps> = ({ data }) => {
+  return <div>cards</div>;
+};
+
+export default ListingCard;
